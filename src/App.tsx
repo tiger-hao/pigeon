24d98191d26e548c3e57fc291b18b71276e3dc4e
Tiger Hao <tigerhao@live.ca>
2019-11-03 21:19:58 -0500

    install bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Routes from "constants/routes";
 import NavigationBar from "components/NavigationBar";
