fe3b79c7edd417aa57fa7689caee5bc3a80c1c9f
Tiger Hao <tigerhao@live.ca>
2019-11-04 20:07:11 -0500

    add signup form validation

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
-import './styles.css';
+import SignupForm from 'components/SignupForm';
 
 const SignupPage = () => {
   return (
@@ -13,27 +13,7 @@ const SignupPage = () => {
       </Row>
       <Row className="justify-content-center">
         <Col md={4}>
-          <Form className="signup">
-            <Form.Group controlId="signup-email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" />
-            </Form.Group>
-            <Form.Group controlId="signup-username">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" />
-            </Form.Group>
-            <Form.Group controlId="signup-password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" />
-            </Form.Group>
-            <Form.Group controlId="signup-confirm-password">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Sign Up
-            </Button>
-          </Form>
+          <SignupForm />
         </Col>
       </Row>
     </Container>
