5d87b6462f33e3f96d69cbef614194fa3f2a317f
Tiger Hao <tigerhao@live.ca>
2020-04-30 20:22:30 -0700

    remove redux ActionCreator type which allows any arguments

--- src/store/user/userActions.ts
+++ src/store/user/userActions.ts
@@ -1,50 +1,49 @@
-import { ActionCreator } from 'redux';
 import {
   IUserLoginInfo, IUserSignupInfo, IUserState, UserActionTypes,
   ILoginRequestAction, ILoginSuccessAction, ILoginFailureAction, ILogoutAction,
   ISignupRequestAction, ISignupSuccessAction, ISignupFailureAction
 } from './userTypes';
 
-export const loginRequest: ActionCreator<ILoginRequestAction> = (loginInfo: IUserLoginInfo) => ({
+export const loginRequest = (loginInfo: IUserLoginInfo): ILoginRequestAction => ({
   type: UserActionTypes.LOGIN_REQUEST,
   payload: {
     loginInfo
   }
 });
 
-export const loginSuccess: ActionCreator<ILoginSuccessAction> = (user: IUserState) => ({
+export const loginSuccess = (user: IUserState): ILoginSuccessAction => ({
   type: UserActionTypes.LOGIN_SUCCESS,
   payload: {
     user
   }
 });
 
-export const loginFailure: ActionCreator<ILoginFailureAction> = (error: Error) => ({
+export const loginFailure = (error: Error): ILoginFailureAction => ({
   type: UserActionTypes.LOGIN_FAILURE,
   payload: {
     error
   }
 });
 
-export const logout: ActionCreator<ILogoutAction> = () => ({
+export const logout = (): ILogoutAction => ({
   type: UserActionTypes.LOGOUT
 });
 
-export const signupRequest: ActionCreator<ISignupRequestAction> = (signupInfo: IUserSignupInfo) => ({
+export const signupRequest = (signupInfo: IUserSignupInfo): ISignupRequestAction => ({
   type: UserActionTypes.SIGNUP_REQUEST,
   payload: {
     signupInfo
   }
 });
 
-export const signupSuccess: ActionCreator<ISignupSuccessAction> = (user: IUserState) => ({
+export const signupSuccess = (user: IUserState): ISignupSuccessAction => ({
   type: UserActionTypes.SIGNUP_SUCCESS,
   payload: {
     user
   }
 });
 
-export const signupFailure: ActionCreator<ISignupFailureAction> = (error: Error) => ({
+export const signupFailure = (error: Error): ISignupFailureAction => ({
   type: UserActionTypes.SIGNUP_FAILURE,
   payload: {
     error
