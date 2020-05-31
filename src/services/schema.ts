4751ed2ed33e6c7e7f5fc775c96c802fda3f08d1
Tiger Hao <tigerhao@live.ca>
2020-05-30 18:16:19 -0700

    create normalizr schema

---
+++ src/services/schema.ts
@@ -0,0 +1,50 @@
+import { schema } from 'normalizr';
+import { Conversation } from './conversationService';
+import { Message } from './messageService';
+import { User } from './userService';
+
+export interface NormalizedMessage {
+  id: Message['id'];
+  sender: string;
+  createdAt: Message['createdAt'];
+  text: Message['text'];
+}
+
+export interface NormalizedMessages {
+  messages: {
+    [key: string]: NormalizedMessage;
+  };
+  users: {
+    [key: string]: User
+  };
+}
+
+export interface NormalizedConversation {
+  id: Conversation['id'];
+  name: Conversation['name'];
+  members: string[];
+  lastMessage: string;
+}
+
+export interface NormalizedConversations {
+  conversations: {
+    [key: string]: NormalizedConversation;
+  };
+  messages: {
+    [key: string]: NormalizedMessage;
+  };
+  users: {
+    [key: string]: User;
+  };
+}
+
+export const userSchema = new schema.Entity<User>('users');
+
+export const messageSchema = new schema.Entity<Message>('messages', {
+  sender: userSchema
+});
+
+export const conversationSchema = new schema.Entity<Conversation>('conversations', {
+  members: [userSchema],
+  lastMessage: messageSchema
+});
